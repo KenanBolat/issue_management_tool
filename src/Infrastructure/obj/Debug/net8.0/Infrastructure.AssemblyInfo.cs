a4db64319e7fa13fd1bb0ef377e8a7094e0bd66c
KenanBolat <kenan23@gmail.com>
2025-10-21 15:53:11 +0300

    first build attempt

--- src/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ src/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01281eb3d4cb486d39d2a13c4838f2e7b3fdc7b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6f993ebd43249f383ae95e873a510a9d5b50966")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
